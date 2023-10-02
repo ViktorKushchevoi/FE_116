0977e1a72074813e49baaa6be8da6bc9a93f32be
Viktor Kushchevoi <victork1609@gmail.com>
2023-10-02 15:06:13 +0300

    Added hw18.

---
+++ hw_18/script.js
@@ -0,0 +1,12 @@
+// one-line comment
+/*
+multi-line
+comment
+*/
+console.log("the second option gives an error")
+function click(){
+    var element=document.getElementsByClassName('className').innerHTML=
+    "<h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h1>";
+  }
+click()
+  
