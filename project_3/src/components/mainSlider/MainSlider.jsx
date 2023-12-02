c016ae55fc0cab59d957a677fffc7357e1d5dd01
Your Name <you@example.com>
2023-12-02 10:24:48 +0200

    fix slider.

--- project_3/src/components/mainSlider/MainSlider.jsx
+++ project_3/src/components/mainSlider/MainSlider.jsx
@@ -1,26 +1,54 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination} from 'swiper/modules';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import React from "react";
 import 'swiper/css'
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import '../../assets/scss/_main_slider.scss';
-import SingleSlide from '../singleSlide/SingleSlide';
+const baseURL = "https://api.themoviedb.org/3";
+const apiKey = "dd365cece93903f23fd8f02821fb9210";
+const allMovies = "/discover/movie";
+const imgBaseURL = "https://image.tmdb.org/t/p/original";
 function MainSlider() {
-    return (
-        <Swiper
-            modules={[Navigation, Pagination]}
-            spaceBetween={50}
-            slidesPerView={1}
-            navigation
-            pagination={{ clickable: true }}
-            onSlideChange={() => console.log('slide change')}
-            onSwiper={(swiper) => console.log(swiper)}
-        >
-            <SwiperSlide><SingleSlide /></SwiperSlide>
-            <SwiperSlide><SingleSlide /></SwiperSlide>
-            <SwiperSlide><SingleSlide /></SwiperSlide>
-            <SwiperSlide><SingleSlide /></SwiperSlide>
-        </Swiper>
-    )
+    const [movies, setMovies] = useState(null);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        axios.get(baseURL + allMovies, {
+            params: {
+                api_key: apiKey,
+            }
+        })
+            .then(response => {
+                setMovies(response.data.results);
+            })
+            .catch(error => {
+                setError(error.message);
+            })
+    }, []);
+    if (movies) {
+        const items = movies.slice(0, 4).map((movie, index) => (
+            <SwiperSlide>
+                <div className="movie-slide">
+                    <img key={index} src={imgBaseURL + movie.backdrop_path}/>
+                </div>
+            </SwiperSlide>
+        ));
+        return (
+            <Swiper
+                modules={[Navigation, Pagination]}
+                spaceBetween={50}
+                slidesPerView={1}
+                loop={true}
+                navigation
+                pagination={{clickable: true}}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+            >
+                {items}
+            </Swiper>
+        )
+    }
 }
 export default MainSlider
